8c294f5570f5bcb38200d8f26a143f4c44f10254
dhauke89 <dhauke89@gmail.com>
2017-11-25 14:49:00 -0500

    Initial commit

---
+++ source/glitch/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+	public float alphaLevel = 1f;
+	public float fadeSpeed;
+	public float waitTime;
+	
+	void Start(){
+		GetComponent<Image>().color = new Color (1,1,1, alphaLevel);
+	}
+	
+	void Update(){
+		if(alphaLevel >0){
+			Invoke("fadeIn", waitTime);
+		}
+		
+	}
+	
+	void fadeIn(){
+		if(alphaLevel > 0){
+			alphaLevel -= fadeSpeed * Time.deltaTime;
+			
+			float newAlphaLevel = Mathf.Clamp(alphaLevel, 0f, 1f);
+			
+			alphaLevel = newAlphaLevel;
+			
+			GetComponent<Image>().color = new Color (1,1,1,alphaLevel);
+		}else if(alphaLevel == 0){
+			Destroy(GetComponent<Image>());
+		}
+	}
+}
